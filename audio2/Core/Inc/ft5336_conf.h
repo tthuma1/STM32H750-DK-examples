fd7494a9c9e82d20fc313084a1d1f9149961ea4e
Tim Thuma <tim.thuma@students.vegova.si>
2026-02-16 23:32:43 +0100

    added more library files to audio2

--- audio2/Core/Inc/ft5336_conf.h
+++ audio2/Core/Inc/ft5336_conf.h
@@ -1,13 +1,13 @@
 /**
   ******************************************************************************
-  * @file    mt48lc4m32b2_conf.h
+  * @file    ft5336_conf.h
   * @author  MCD Application Team
-  * @brief   This file contains some configurations required for the
-  *          MT48LC4M32B2 SDRAM memory.
+  * @brief   This file contains specific configuration for the
+  *          ft5336.c that can be modified by user.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2015 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -19,55 +19,24 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef MT48LC4M32B2_CONF_H
-#define MT48LC4M32B2_CONF_H
+#ifndef FT5336_CONF_H
+#define FT5336_CONF_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-/* Update the below line with adequate hal file. ie: #include "stm32f7xx_hal.h" */
-#include "stm32h7xx_hal.h"
-   
-/** @addtogroup BSP
-  * @{
-  */
+/* Macros --------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+#define FT5336_AUTO_CALIBRATION_ENABLED      0U
+#define FT5336_MAX_X_LENGTH                  800U
+#define FT5336_MAX_Y_LENGTH                  480U
   
-/** @addtogroup Components
-  * @{
-  */
-
-/** @addtogroup MT48LC4M32B2
-  * @{
-  */
-
-/** @addtogroup MT48LC4M32B2_Exported_Constants
-  * @{
-  */  
-#define REFRESH_COUNT                    ((uint32_t)0x0603)   /* SDRAM refresh counter */
-   
-#define MT48LC4M32B2_TIMEOUT             ((uint32_t)0xFFFF)
-
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* mt48lc4m32b2_CONF_H */
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
+#endif /* FT5336_CONF_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
