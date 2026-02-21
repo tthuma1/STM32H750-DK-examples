f377e362eef8bd2f8920992af038137544651ea8
Tim Thuma <tim.thuma@students.vegova.si>
2026-02-21 15:45:35 +0100

    more simplifications for record demo

--- BSP/Src/main.c
+++ BSP/Src/main.c
@@ -33,14 +33,9 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-uint8_t DemoIndex = 0;
 
 /* Volume of the audio playback */
 /* Initial volume level (from 0% (Mute) to 100% (Max)) */
-__IO uint8_t volume = 60;
-__IO uint8_t VolumeChange = 0;
-__IO uint32_t ButtonState = 0;
-__IO uint32_t SRAMTest = 0;
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void MPU_Config(void);
@@ -76,51 +71,16 @@ ALIGN_32BYTES (uint16_t  PlaybackBuffer[2*AUDIO_BUFF_SIZE]);
 /* Pointer to record_data */
 uint32_t playbackPtr;
 
-uint32_t  InState = 0;
-uint32_t  OutState = 0;
+// uint32_t  InState = 0;
 
 uint32_t AudioBufferOffset;
-uint32_t PlaybackStarted = 0;
 BSP_AUDIO_Init_t  AudioInInit;
 BSP_AUDIO_Init_t  AudioOutInit;
 BSP_AUDIO_Init_t AnalogInInit;
 
-static uint32_t AudioFreq[9] = {8000 ,11025, 16000, 22050, 32000, 44100, 48000, 96000, 192000};
-uint32_t *AudioFreq_ptr;
+// static uint32_t AudioFreq[9] = {8000 ,11025, 16000, 22050, 32000, 44100, 48000, 96000, 192000};
+// uint32_t *AudioFreq_ptr;
 
-uint32_t VolumeLevel = 80, AudioOutState, AudioInState, MuteState;
-char text[256];
-char* Audio_Out_State[4] = {
-  " AUDIO_OUT_STATE_RESET  ",
-  " AUDIO_OUT_STATE_PLAYING",
-  " AUDIO_OUT_STATE_STOP   ",
-  " AUDIO_OUT_STATE_PAUSE  "
-};
-
-char* Audio_In_State[4] = {
-  "AUDIO_IN_STATE_RESET    ",
-  "AUDIO_IN_STATE_RECORDING",
-  "AUDIO_IN_STATE_STOP     ",
-  "AUDIO_IN_STATE_PAUSE    "
-};
-
-char* Mute_State[2] = {
-  " BSP_AUDIO_MUTE_DISABLED",
-  " BSP_AUDIO_MUTE_ENABLED ",
-};
-
-
-BSP_DemoTypedef  BSP_examples[]=
-{
-  // {Touchscreen_demo1, "TOUCHSCREEN DEMO1", 0},
-  // {Touchscreen_demo2, "TOUCHSCREEN DEMO2", 0},
-  // {LCD_demo, "LCD", 0},
-  // {AudioPlay_demo, "AUDIO PLAY", 0},
-  {AudioRecord_demo, "AUDIO RECORD", 0},
-  // {MMC_demo, "MMC", 0},
-  // {SDRAM_demo, "SDRAM", 0},
-  // {SDRAM_DMA_demo, "SDRAM MDMA", 0}
-};
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -169,25 +129,21 @@ int main(void)
 
   uint32_t channel_nbr = 2;
 
-  uint32_t x_size, y_size;
-
-  AudioFreq_ptr = AudioFreq+2; /* AUDIO_FREQUENCY_16K; */
-
   AudioOutInit.Device = AUDIO_OUT_DEVICE_HEADPHONE;
   AudioOutInit.ChannelsNbr = channel_nbr;
-  AudioOutInit.SampleRate = *AudioFreq_ptr;
+  AudioOutInit.SampleRate = AUDIO_FREQUENCY;
   AudioOutInit.BitsPerSample = AUDIO_RESOLUTION_16B;
-  AudioOutInit.Volume = VolumeLevel;
+  AudioOutInit.Volume = 80;
 
   AudioInInit.Device = AUDIO_IN_DEVICE_DIGITAL_MIC;
   AudioInInit.ChannelsNbr = channel_nbr;
-  AudioInInit.SampleRate = *AudioFreq_ptr;
+  AudioInInit.SampleRate = AUDIO_FREQUENCY;
   AudioInInit.BitsPerSample = AUDIO_RESOLUTION_16B;
-  AudioInInit.Volume = VolumeLevel;
+  AudioInInit.Volume = 80;
 
   /* Initialize Audio Recorder with 2 channels to be used */
   BSP_AUDIO_IN_Init(1, &AudioInInit);
-  BSP_AUDIO_IN_GetState(1, &InState);
+  // BSP_AUDIO_IN_GetState(1, &InState);
 
   BSP_AUDIO_OUT_Init(0, &AudioOutInit);
 
@@ -202,20 +158,6 @@ int main(void)
   while (1)
   {
 
-    // if(ButtonState == 1)
-    // {
-    //   HAL_Delay(400);
-    //   ButtonState = 0;
-      BSP_examples[DemoIndex++].DemoFunc();
-
-    //   HAL_Delay(100);
-
-    //   if(DemoIndex >= COUNT_OF_EXAMPLE(BSP_examples))
-    //   {
-    //     DemoIndex = 0;
-    //   }
-    //   Display_DemoDescription();
-    // }
   }
 }
 /**
